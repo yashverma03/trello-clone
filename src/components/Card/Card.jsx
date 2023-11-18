e0cfabcc289dd53d3d5a2498a17be711074af3f6
yashverma03 <vermayash2003@gmail.com>
2023-11-18 14:13:06 +0530

    created Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,43 @@
 import styles from './Card.module.css';
+import userIcon from '../../assets/userIcon.png';
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ id, title, tag }) => {
+  const {
+    container,
+    titleWrap,
+    titleId,
+    userImage,
+    description,
+    detailsWrap,
+    details,
+    exclamationMark,
+    tagWrap,
+    circle,
+    detail
+  } = styles;
+
+  return (
+    <main className={container}>
+      <div className={titleWrap}>
+        <p className={titleId}>{id}</p>
+        <img className={userImage} src={userIcon} alt='user icon' />
+      </div>
+
+      <div className={description}>{title}</div>
+
+      <div className={detailsWrap}>
+        <div className={details}>
+          <p className={exclamationMark}>!</p>
+        </div>
+        <div className={details}>
+          <div className={tagWrap}>
+            <div className={circle} />
+            <p className={detail}>{tag[0]}</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Card;
