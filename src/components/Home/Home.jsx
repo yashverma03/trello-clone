158ecceb95ce1cbc2d097f435fa1fd0fb8da4ac9
yashverma03 <vermayash2003@gmail.com>
2023-11-18 14:14:25 +0530

    render single Card component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,20 @@
+import Card from '../Card/Card';
 import styles from './Home.module.css';
 
+const temp = {
+  id: 'CAM-1',
+  title: 'Update User Profile Page UI',
+  tag: ['Feature Request']
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  const { container } = styles;
+
+  return (
+    <main className={container}>
+      <Card id={temp.id} title={temp.title} tag={temp.tag} />
+    </main>
+  );
 };
 
 export default Home;
